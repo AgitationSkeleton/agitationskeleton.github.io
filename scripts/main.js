25a53e3167b014d56dde35982d778cafbecd617f
AgitationSkeleton <alphablaster1337@gmail.com>
2023-07-05 12:20:14 -0700

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 	get: (searchParams, prop) => searchParams.get(prop),
 });
 var gameMode = params.game;
+var schizo = 0;
 
 document.addEventListener('DOMContentLoaded', (event) => {
 	if (gameMode == 1) {
@@ -13,6 +14,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	}
 
 	document.getElementById("headerNav").addEventListener('click', navToggle);
+
+	// schizo check
+	var path = window.location.pathname;
+	var page = path.split("/").pop();
+	if ((page == "secretbase") || (page == "secretbase.html")) schizo = 1;
+	if (schizo) {
+		document.getElementById("yoda").classList.add("creepypastaGlow");
+		document.getElementById("yodaImage").classList.add("creepypastaShaker");
+	}
+
 	document.getElementById("yodaImage").addEventListener('click', yoda);
 	document.getElementById("yodaText").addEventListener('click', yodaHide);
 });
@@ -29,11 +40,31 @@ function navToggle() {
 
 // preload sounds
 const yodaSound = new Audio("sounds/yodalaugh.mp3");
+const yodaSoundCreepy = new Audio("sounds/yodacreepy.mp3");
 
 function yoda() {
-	yodaSound.load();
-	yodaSound.play();
-	jQuery.get('yodas_wisdom.txt', function(fulltext) {
+	(schizo ? yodaSoundCreepy : yodaSound).load();
+	(schizo ? yodaSoundCreepy : yodaSound).play();
+	jQuery.get((schizo ? 'yodas_thoughts.txt' : 'yodas_wisdom.txt'), function(fulltext) {
+		// schizo execution
+		if (schizo) {
+			document.getElementById("yodaImage").src = "images/creepypasta/yoda.gif";
+	
+			document.getElementById("title").classList.add("creepypastaGlitch");
+			document.getElementById("headerBackground").classList.add("creepypastaHeader");
+			document.getElementById("gameLogo").classList.add("creepypastaLogo");
+			document.getElementById("gameLogo").src = "images/creepypasta/tfclogo.png";
+			document.getElementById("contentInner").classList.add("creepypastaBackground");
+			document.getElementById("contentText").innerHTML = "";
+			document.querySelectorAll(".contentHeader").forEach((contentHeader) => {
+				contentHeader.innerHTML = "YOU ARE BEING MONITORED"
+			});
+			document.getElementById("footerInner").innerHTML = "TURN BACK";
+			document.querySelectorAll("#navInner a").forEach((navLink) => {
+				navLink.innerHTML = "›RUN"
+			});
+		}
+
 		var seed = Math.random();
 		var quotes = fulltext.split(/\r?\n/);
 		var quotenum = Math.round(seed*(quotes.length-1));
