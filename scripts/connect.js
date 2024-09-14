9d16766a9bcd4c7c45808c580c3801157e23fdc5
Boba <trashboyjet@gmail.com>
2024-09-14 14:13:19 -0500

    connect cleanup by sharkobarko

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,15 +1,11 @@
-var params = [];
-for (const key of new URLSearchParams(window.location.search).keys()) {
-	params.push(key);
-}
-var ip = params[0];
+const ip = new URLSearchParams(window.location.search).keys().next().value;
 const regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::(?:[1-9]\d{0,3}|[1-5]\d{4}|6(?:[0-4]\d{3}|5(?:[0-4]\d{2}|5(?:[0-2]\d|3[0-5])))))?$/;
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
 	if (regex.test(ip)) {
-		window.location.replace("steam://connect/"+ip);
+		window.location.href = `steam://connect/${ip}`;
 		window.location.replace("https://redchanit.xyz/servers");
 	} else {
 		window.location.replace("https://redchanit.xyz/404");
-	}
+    }
 });
